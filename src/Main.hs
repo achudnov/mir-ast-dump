351f441e191a3841a3695a589fd2321ad0de0163
Andrey Chudnov <andrey@chudnov.com>
2018-02-06 18:25:38 -0800

    Version 0.1

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,24 @@
 module Main where
 
 import Text.Groom
+import Mir.Mir
+import qualified Data.Aeson as J
+import qualified Data.ByteString.Lazy as B
+import Mir.Pass.CollapseRefs as P
+import Mir.Pass.RewriteMutRef as P
+import Mir.Pass.RemoveStorage as P
+import Mir.Pass.RemoveBoxNullary as P
 
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do f <- B.readFile ""
+          let c = (J.eitherDecode f) :: Either String Collection
+          case c of
+            Left msg -> fail $ "Decoding of MIR failed: " ++ msg
+            Right coll -> do
+              --let passes = P.passMutRefArgs . P.passRemoveStorage . P.passRemoveBoxNullary
+              let passes = P.passRemoveBoxNullary
+              let fns = passes (functions coll)
+              -- DEBUGGING print functions
+              -- mapM_ (putStrLn . pprint) fns
+              putStrLn $ groom $ fns
